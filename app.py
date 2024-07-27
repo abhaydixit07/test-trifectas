c14f45e6cefaa9173e2e574c4ef40196934a3b63
abhaydixit07 <abhaydixit227@gmail.com>
2024-07-27 15:07:48 +0530

    Test

--- app.py
+++ app.py
@@ -211,5 +211,9 @@ def generate_word():
     print(COUPLED)
     return jsonify(word_data)
 
+@app.route("/")
+def home():
+    return "Hello, World!"
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
